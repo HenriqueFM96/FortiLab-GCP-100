48db1bf4086b4f8f2bd1b5e22aac84c57b16fd2d
Henrique Moises <hmoises@fortinet-us.com>
2022-09-07 15:03:12 -0300

    commit with VPC only

--- main.tf
+++ main.tf
@@ -7,37 +7,36 @@ terraform {
   }
 }
 
-/*
-provider "google" {
-  #credentials = file(terraform.workspace)
 
-  project = var.project
-  region  = var.region
-  zone    = var.zone
-}
-*/
+#provider "google" {
+#  #credentials = file(terraform.workspace)
+#
+#  project = var.project
+#  region  = var.region
+#  zone    = var.zone
+#}
+#
 
 resource "google_compute_network" "vpc_network" {
   name        = "fortilab-gcp-100-vpc"
   description = "FortiLab VPC - GCP 100"
   region      = var.region
 }
-/*
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
-  tags         = ["web", "dev"]
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
+#resource "google_compute_instance" "vm_instance" {
+#  name         = "terraform-instance"
+#  machine_type = "f1-micro"
+#  tags         = ["web", "dev"]
+#
+#  boot_disk {
+#    initialize_params {
+#      image = "debian-cloud/debian-9"
+#    }
+#  }
 
-  network_interface {
-    network = google_compute_network.vpc_network.name
-    access_config {
-    }
-  }
-}
-*/
+#  network_interface {
+#    network = google_compute_network.vpc_network.name
+#    access_config {
+#    }
+#  }
+#}
